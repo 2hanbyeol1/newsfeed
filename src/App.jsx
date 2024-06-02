11e65bf60cadea068211da5faf3e3dd0428b4562
heesu21 <gmltncpfl1@naver.com>
2024-06-02 23:00:15 +0900

    feat: 메인페이지 - ui, ux 작업 - 스크롤 업 기능 - 뉴스피드 컴포넌트화

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle";
 import Main from "./pages/Main";
+import UpButton from "./component/UpButton";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
+      <UpButton />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,5 +1,17 @@
-function Main() {
-  return <div>메인페이지</div>;
-}
+import styled from "styled-components";
+import NewsFeed from "../../component/NewsFeed";
+
+const Main = () => {
+  const Container = styled.div`
+    display: flex;
+    justify-content: center;
+  `;
+
+  return (
+    <Container>
+      <NewsFeed />
+    </Container>
+  );
+};
 
 export default Main;
