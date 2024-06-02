ae2a474ff61a206ca34329815be855a453ad9197
leeboa2005 <leeboa2005@naver.com>
2024-06-02 15:10:56 +0900

    init : branch 생성 및 Write 페이지 레이아웃 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle";
 import Main from "./pages/Main";
+import Detail from "./pages/Detail";
+import Write from "./pages/Write";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/detail/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
