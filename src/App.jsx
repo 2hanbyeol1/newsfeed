49087cd39e3690edc7cd9f8938e08b5f2e476cc5
2hanbyeol1 <2hanbyeol1@naver.com>
2024-06-03 10:16:18 +0900

    feat: 로그인 화면

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle";
+import Login from "./pages/Login";
 import Main from "./pages/Main";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
