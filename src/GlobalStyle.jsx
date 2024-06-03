9f5f18906b15ee1f0d6b40032c91df3702839130
2hanbyeol1 <2hanbyeol1@naver.com>
2024-06-03 10:24:44 +0900

    feat: 폰트 추가

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,7 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+
 const GlobalStyle = createGlobalStyle`
+	@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap');
 	${reset}
+
+	* {
+		font-family: "Noto Sans KR", sans-serif !important;
+		font-style: normal !important;
+	}
 `;
 
 export default GlobalStyle;
