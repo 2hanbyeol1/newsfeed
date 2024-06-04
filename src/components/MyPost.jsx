818d10f108598a371e915a91d0a394c11ba60f33
junghyunwoo02 <jungilbung@naver.com>
2024-06-04 16:28:30 +0900

    refactor: MyPost컴포넌트 더미포스트 삭제, API 호출 MyPage로 리팩토링

--- src/components/MyPost.jsx
+++ src/components/MyPost.jsx
@@ -1,73 +1,24 @@
 import styled from "styled-components";
 
-const dummyPosts = [
-  {
-    id: 1,
-    title: "5월 4주차 TOP 10",
-    description: "[TOP 10] 개발자에게 도움이 될 만한...",
-    imageUrl: "https://velog.velcdn.com/images/oneoneone/post/1b46c1d0-c393-4f6a-b2c3-1dc33ea5fa85/image.png"
-  },
-  {
-    id: 2,
-    title: "난 주니어 개발자다",
-    description: "주니어 개발자의 이야기를 담은...",
-    imageUrl:
-      "https://media.licdn.com/dms/image/sync/D5627AQE3g3IxscKSWg/articleshare-shrink_800/0/1712043794313?e=2147483647&v=beta&t=GVXU-SfLHlHXX2Dg1EkRb6wRSmou5mDOUen_fnwCwUE"
-  },
-  {
-    id: 3,
-    title: "새로운 클라우드 구축하기",
-    description: "클라우드 인프라를 구축하는 방법...",
-    imageUrl: "https://i.ytimg.com/vi/Br2Z5wCk98o/maxresdefault.jpg"
-  },
-  {
-    id: 4,
-    title: "AI 산업에 있어서 앞으로의 난관",
-    description: "대충 주가 떨어지는 소리가...",
-    imageUrl:
-      "https://img.freepik.com/free-vector/businessman-feeling-down-about-economic-crisis-caused-by-coronavirus-vector_53876-169103.jpg?w=1380&t=st=1717247942~exp=1717248542~hmac=309975c34ed043cd68461d30260b849cff10713b91cf1d3884b12c61a5aae68f"
-  },
-  {
-    id: 5,
-    title: "리액트에서 useEffect에 대한 고찰",
-    description: "컴포넌트의 여러 값들을 활용해 ...",
-    imageUrl: "https://velog.velcdn.com/images/alsgud8311/post/b921f9e8-3153-4092-9fff-cc13691cd805/image.jpg"
-  },
-  {
-    id: 6,
-    title: "최근 개발자 취업에 대해서",
-    description: "요즘 취업시장이 정말 얼음장 같은...",
-    imageUrl:
-      "https://oopy.lazyrockets.com/api/v2/notion/image?src=https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fe3095240-9720-4e92-a88b-0d24d2a649e2%2F%E1%84%91%E1%85%B3%E1%84%85%E1%85%A6%E1%84%8C%E1%85%A6%E1%86%AB%E1%84%90%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%89%E1%85%A7%E1%86%AB1.jpg&blockId=756956c2-9d68-4e62-b84e-e36b39006e3c"
-  }
-];
-
-const MyPost = () => {
-  const handleEdit = (postId) => {
-    console.log(`Edit post with id: ${postId}`);
-    // 추가 로직 작성
-  };
-
-  const handleDelete = (postId) => {
-    console.log(`Delete post with id: ${postId}`);
-    // 추가 로직 작성
-  };
+const MyPost = ({ users }) => {
   return (
     <StPosts>
       <h3>내 게시글</h3>
-      <StPostGrid>
-        {dummyPosts.map((post) => (
-          <StPost key={post.id}>
-            <img src={post.imageUrl} alt="Post" />
-            <h4>{post.title}</h4>
-            <p>{post.description}</p>
-            <StButtonGroup>
-              <button onClick={() => handleEdit(post.id)}>수정</button>
-              <button onClick={() => handleDelete(post.id)}>삭제</button>
-            </StButtonGroup>
-          </StPost>
-        ))}
-      </StPostGrid>
+      {users.map((user) => (
+        <div key={user.id}>
+          <StPostGrid>
+            {user.posts.map((post) => (
+              <StPost key={post.id}>
+                <img src={post.image_url} alt="Post" />
+                <h4>{post.title}</h4>
+                <p>{post.description}</p>
+                <p>{new Date(post.created_at).toLocaleDateString()}</p>
+                <h2>{user.nickname}</h2>
+              </StPost>
+            ))}
+          </StPostGrid>
+        </div>
+      ))}
     </StPosts>
   );
 };
@@ -121,38 +72,5 @@ const StPost = styled.div`
   p {
     font-size: 0.9em;
     color: #666;
-  }
-`;
-
-const StButtonGroup = styled.div`
-  margin-top: 10px;
-
-  button {
-    flex: 1;
-    padding: 8px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 0.9em;
-    transition: background-color 0.3s;
-
-    &:first-child {
-      margin-right: 10px;
-      background-color: #b3b3b3;
-      color: white;
-
-      &:hover {
-        background-color: #8e8d8d;
-      }
-    }
-
-    &:last-child {
-      background-color: #f44336;
-      color: white;
-
-      &:hover {
-        background-color: #e43431;
-      }
-    }
   }
 `;
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -2,17 +2,52 @@ import styled from "styled-components";
 import Header from "../components/Header";
 import MyInfo from "../components/MyInfo";
 import MyPost from "../components/MyPost";
-import { Outlet } from "react-router-dom";
+
+import { useEffect, useState } from "react";
+import supabase from "../supabase/supabase";
 
 const MyPage = () => {
+  const [users, setUsers] = useState([]);
+  const userUuid = "d2ae74c5-3d4c-4df3-befa-334170bc9394";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data: usersData, error } = await supabase
+        .from("Users")
+        .select(
+          `
+      id,
+      nickname,
+      profile_image,
+      posts (
+        id,
+        title,
+        description,
+        created_at,
+        created_by,
+        image_url
+      )
+`
+        )
+        .eq("id", userUuid);
+
+      if (error) {
+        console.log("error => ", error);
+      } else {
+        console.log("Users => ", usersData);
+        setUsers(usersData);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <StContainer>
       <Header />
       <main>
-        <MyInfo />
-        <MyPost />
+        <MyInfo users={users} />
+        <MyPost users={users} />
       </main>
-      <Outlet />
     </StContainer>
   );
 };
