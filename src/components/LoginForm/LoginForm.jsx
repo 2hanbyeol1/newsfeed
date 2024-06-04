3b52e4c260ff4ddd39aa00c5be1bb62279e4abc1
minseok <minskyson7@gmail.com>
2024-06-04 17:28:00 +0900

    feat 회원가입 기능

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -11,17 +11,17 @@ const StyledForm = styled.form`
 `;
 
 function LoginForm() {
-  const [{ id, pw }, onChange] = useInputs({ id: "", pw: "" });
+  const [{ email, pw }, onChange] = useInputs({ email: "", pw: "" });
 
   const handleLoginFormSubmit = (e) => {
     e.preventDefault();
-    if (id === "") return alert("id를 입력해주세요");
+    if (email === "") return alert("email을 입력해주세요");
     if (pw === "") return alert("pw를 입력해주세요");
   };
 
   return (
     <StyledForm onSubmit={handleLoginFormSubmit}>
-      <Input placeholder="id" name="id" value={id} onChange={onChange} />
+      <Input placeholder="email" name="email" value={email} onChange={onChange} />
       <Input type="password" placeholder="pw" name="pw" value={pw} onChange={onChange} />
       <Button>Login</Button>
     </StyledForm>
