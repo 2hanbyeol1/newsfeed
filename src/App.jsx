97a53f23c2e6d01deb99041036da8d67773b86d9
leeboa2005 <leeboa2005@naver.com>
2024-06-04 10:18:20 +0900

    feat : Write branch 생성 및 레이아웃 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle";
+import Layout from "./components/Layout";
 import UpButton from "./components/UpButton";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
 import MyPage from "./pages/MyPage";
 import SignUp from "./pages/SignUp/SignUp";
+import Write from "./pages/Write";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <UpButton />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/mypage" element={<MyPage />} />
-      </Routes>
+      <Layout>
+        <UpButton />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/write" element={<Write />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
